db3a62dcbaf7762993aa7719fa91d65a498713a0
Zaid Rahawi <zaid.riadh@gmail.com>
2018-08-16 18:26:02 +0300

    Adapt to latest macos-tools changes

--- Y520.sh
+++ Y520.sh
@@ -75,7 +75,10 @@ case "$1" in
     ;;
     --install-essential-kexts)
     	macos-tools/unarchive_file.sh -d $kexts_dir
-        macos-tools/install_kext.sh -i $(findKext FakeSMC.kext) $(findKext RealtekRTL8111.kext) $(findKext FakePCIID.kext) $(findKext FakePCIID_Broadcom_WiFi.kext) $(findKext USBInjectAll.kext) $(findKext ACPIBatteryManager.kext) $(findKext VoodooPS2Controller.kext)
+        EFI=$(macos-tools/mount_efi.sh)
+        kext_dest=$EFI/EFI/CLOVER/kexts/Other
+        rm -Rf $kext_dest/*.kext
+        macos-tools/install_kext.sh -s $kext_dest $(findKext FakeSMC.kext) $(findKext RealtekRTL8111.kext) $(findKext FakePCIID.kext) $(findKext FakePCIID_Broadcom_WiFi.kext) $(findKext USBInjectAll.kext) $(findKext ACPIBatteryManager.kext) $(findKext VoodooPS2Controller.kext)
     ;;
     --install-hdainjector)
         macos-tools/create_hdainjector.sh -c $hda_codec -r $hda_resources -o $local_kexts_dir
